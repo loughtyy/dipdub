fd1d1064a222f796efc75e343d6ed7c3a18d9b2c
loughtyy <zizamq@gmail.com>
2026-05-11 02:37:36 +0300

    loughtyy

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,13 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'jewelryshop/static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'jewelryshop/static'),
+    os.path.join(BASE_DIR, 'media'),              
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-STATICFILES_DIRS.append(MEDIA_ROOT)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
