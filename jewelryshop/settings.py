8fe2708e1f184652747037e8d0a4733f8677e6d1
loughtyy <zizamq@gmail.com>
2026-05-11 01:57:30 +0300

    loughtyy

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -118,13 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'jewelryshop/static'), MEDIA_ROOT]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static') # Automatically Created on Production
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Settings for Media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'jewelryshop/static'), MEDIA_ROOT]
 
 LOGOUT_REDIRECT_URL = '/'
 
