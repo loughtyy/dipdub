38bf1d8221a2dee00d4ea2a45cec1b7795e525fc
loughtyy <zizamq@gmail.com>
2026-05-11 03:06:12 +0300

    loughtyy

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/static/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
