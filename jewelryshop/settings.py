ca9530b7744209245fc76a81f21859ac8b019949
loughtyy <zizamq@gmail.com>
2026-06-15 14:23:00 +0300

    loughtyy

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -119,16 +119,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'jewelryshop/static'),  
-]   
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'jewelryshop/static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/static/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-STATICFILES_DIRS.append(MEDIA_ROOT)
 
 
 import sys
